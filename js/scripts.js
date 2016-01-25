20035c5c1f2fba35dc069f8576f2d5b7cdbd2004
Jackson Cafazzo <doctorjuno@gmail.com>
2016-01-25 00:14:38 -0800

    wheeeee

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 function initMap() {
+  debugger;
+
   var map;
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
 }
-
-$(document).ready(function() {
-  async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9m1ReIk_LMoa3zaAzw1EnfuHzQsrnJfg&callback=initMap">
-
-  event.preventDefault();
-}
+// 
+// $(document).ready(function() {
+//
+//   event.preventDefault();
+// )};
---
+++ specs/specs.js
@@ -0,0 +1,6 @@
+describe("initMap", function () {
+  it("gets a map from google APIs based on lat/long", function () {
+    var newMap;
+    expect("")
+  })
+})
